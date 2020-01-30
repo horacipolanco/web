7bae48a42dfb199b7e12fad8abf53996c4421102
horacipolanco <horacipolanco@gmail.com>
2020-01-30 18:23:18 +0100

    Update hpapp.js

--- hpapp.js
+++ hpapp.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   
     changeWords();
+    changeWordsSecond();
 
 function changeWords() {
   var words = $("h1.heading-jumbo span");
@@ -10,6 +11,16 @@ function changeWords() {
       $(words).removeClass("active");
       $(words).eq(i).addClass("active");
   }, 2000);
+}
+  
+  function changeWordsSecond() {
+  var words = $("h1.heading-jumbo span.second");
+  var i = 0;
+  setInterval(function () {
+      i = i < words.length - 1 ? i+1 : 0;
+      $(words).removeClass("activesecond");
+      $(words).eq(i).addClass("activesecond");
+  }, 4000);
 }
 
 });
